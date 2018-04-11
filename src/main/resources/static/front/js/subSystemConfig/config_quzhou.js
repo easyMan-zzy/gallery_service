a77ec4dfd342a4236725530025ea1b335728625c
easyMan-zzy <272210465@qq.com>
2018-04-11 20:59:00 +0800

    增加衢州js配置文件

---
+++ src/main/resources/static/front/js/subSystemConfig/config_quzhou.js
@@ -0,0 +1,14 @@
+var config_city = '衢州';
+var config_city_center = [118.866597, 28.975546];
+var config_sysLogo = "img/yindaologo.png";
+var config_sysName = "科普画廊动态管理及资源共享系统(浙江省衢州市)";
+var config_sysSlides = ["img/slider0.png","img/slider1.png","img/slider2.png","img/slider3.png"];
+var config_zhuban = [{name:"浙江省科学技术协会",url:"http://www.zast.org.cn/"},{name:"衢州市科学技术协会",url:"http://www.qzkx.gov.cn/"}];
+var config_manageMethod = "衢州市科协科普画廊管理办法";
+var config_manageDesc = '为贯彻落实市委、市政府关于加快文化大市建设和创业富民、创新强市的战略部署，推进科学技术普及，提升基层科普服务能力，提高公民科学素质，更好地引导全市科普画廊的发展。';
+var config_manageContent = '';
+var config_imageUrl = "http://localhost";
+var config_houtaiLogin = "http://localhost:8080/background/login.html";
+var config_rest_url = "http://localhost:8080";
+
+var config_displayPanel = "大众科普服务中心在全大市中的其中9个县（市）区、街道、学校、企业、社区、村，承担着1835个画廊的展板更换和信息服务工作。画廊展板、挂图制作以全市近百家学会、协会为智力支撑；专业的科普作品研究制作团队，高效的社区服务，广博的科普文化知识直抵家门，让精彩科普为您的生活增添乐趣。";
