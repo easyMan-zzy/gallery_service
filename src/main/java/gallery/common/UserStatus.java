4067b44338581a65104aaf875f8726b326c35422
zman <272210465@qq.com>
2019-05-27 10:13:33 +0800

    之前保留的，提交为了同步

--- src/main/java/gallery/common/UserStatus.java
+++ src/main/java/gallery/common/UserStatus.java
@@ -9,19 +9,19 @@ public enum UserStatus {
     private UserStatus(String name, Long index) {
         this.name = name;  
         this.index = index;  
-    }  
+    }
 
     // get set 方法  
     public String getName() {  
         return name;  
-    }  
+    }
     public void setName(String name) {  
         this.name = name;  
-    }  
+    }
     public Long getIndex() {
         return index;  
-    }  
+    }
     public void setIndex(Long index) {
         this.index = index;  
-    }  
+    }
 }  
